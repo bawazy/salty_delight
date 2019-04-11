5295f41ee7590e76f1188b6380cec511ca8762d0
hayatu <hbawazy@gmail.ccom>
2019-04-11 14:55:37 +0100

    displayed price and trying to work on Cart

--- src/components/menu.js
+++ src/components/menu.js
@@ -13,15 +13,18 @@ export class Menu extends Component {
                             {
                               img:'https://thumbs.dreamstime.com/z/pepperoni-pizza-thinly-sliced-popular-topping-american-style-pizzerias-30402134.jpg',
                               itemName:'Pizza 1',
+                              price:3000,
                             },
 
                             {
                               img:'https://www.bars.digital/data/users/2/dishes/319/images/0_31746800_1526641380.jpg',
                               itemName:'Pizza 2',
+                              price:3500,
                             },
                             {
                               img:'https://www.tasteofhome.com/wp-content/uploads/2017/10/Chicken-Pizza_exps30800_FM143298B03_11_8bC_RMS-2.jpg',
                               itemName:'Pizza 3',
+                              price:2000,
                             },
 
 
@@ -36,15 +39,18 @@ export class Menu extends Component {
                               {
                                 img:'https://www.thespruceeats.com/thmb/gSU7cxrCTiqNHpGHVKdF-Eavo2o=/450x0/filters:no_upscale():max_bytes(150000):strip_icc()/southern-oven-fried-chicken-3058647-5_preview-5b0ec6ecba61770036491ed7.jpeg',
                                 itemName:'Chicken 1',
+                                price:1500,
                               },
 
                               {
                                 img:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRuRd-wYiyNuYo_TJbviLrMsjZzV7Irvd8lF-PeZ0uqDamgJX_z',
                                 itemName:'Chicken 2',
+                                price:2000,
                               },
                               {
                                 img:'https://www.maangchi.com/wp-content/uploads/2016/10/honeybutterchicken-insta.jpg',
                                 itemName:'Chicken 3',
+                                price:500,
                               },
         ],
       },
@@ -56,15 +62,18 @@ export class Menu extends Component {
                                 {
                                   img:'https://images.pexels.com/photos/70497/pexels-photo-70497.jpeg?cs=srgb&dl=burger-chips-dinner-70497.jpg&fm=jpg',
                                   itemName:'Food 1',
+                                  price:500,
                                 },
 
                                 {
                                   img:'https://www.sbs.com.au/topics/sites/sbs.com.au.topics/files/styles/full/public/gettyimages-469640958.jpg?itok=96jW1o_l&mtime=1475625718',
                                   itemName:'Food 2',
+                                  price:600,
                                 },
                                 {
                                   img:'https://data.whicdn.com/images/8411081/original.jpg',
                                   itemName:'Food 3',
+                                  price:700,
                                 },
         ],
       }
@@ -99,7 +108,7 @@ export class Menu extends Component {
     // console.log(this.state.menus)
     return (
 
-    <div>
+    <div className='wrapper'>
         <div className='menus'>    
         {!isSubItems ? (
           <Fragment>
@@ -113,7 +122,7 @@ export class Menu extends Component {
           <Fragment>
           <h1>{food}</h1>
           {activeSubItems.map((sub, index) => {
-            return <SubItem key={index} index={index} itemName={sub.itemName} img={sub.img}/>
+            return <SubItem key={index} index={index} itemName={sub.itemName} img={sub.img} price={sub.price}/>
            }   
           )}
           
@@ -122,14 +131,12 @@ export class Menu extends Component {
         )}
         </div>
         
-{/* 
-        <ul>
-        {this.state.menus.subItems.map(subItem =>{
-          return <SubItem title ={subItem.title}/>
-        })}
-        </ul>
-       */}
+          <div className='cart'>
 
+
+
+
+          </div>
 
     </div>
     )
--- src/components/subItem.js
+++ src/components/subItem.js
@@ -6,9 +6,13 @@ export class SubItem extends Component {
        <div className='menu-item'>
       <div className='subItem' > 
        <h3>{this.props.itemName}</h3>
-        <img src={this.props.img} alt='' /> 
-        <button className='orderBtn'>Order Now</button>
+        <img src={this.props.img} alt='' />   
+        <button  className='orderBtn'>Order Now</button>
+        <h3> price:{this.props.price}</h3>
         </div>
+
+
+
       </div>
     )
   }
