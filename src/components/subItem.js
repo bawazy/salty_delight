3ae09f49de6584a4779430cecbfa969bef9581e2
hayatu <hbawazy@gmail.ccom>
2019-04-11 06:48:23 +0100

    h

--- src/components/subItem.js
+++ src/components/subItem.js
@@ -3,10 +3,10 @@ import React, { Component } from 'react'
 export class SubItem extends Component {
   render() {
     return (
-      <div className='subItem' >
-        <div className='menu-item'>  
-        <img src='' alt='' />
-        {this.props.itemName}
+       <div className='menu-item'>
+      <div className='subItem' > 
+       <h3>{this.props.itemName}</h3>
+        <img src='https://thumbs.dreamstime.com/z/pepperoni-pizza-thinly-sliced-popular-topping-american-style-pizzerias-30402134.jpg' alt='' /> 
         <button className='orderBtn'>Order Now</button>
         </div>
       </div>
